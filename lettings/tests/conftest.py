216f049ce13dc70c6a1ebe22e418d687a73cf153
Victor <codingvictor22@gmail.com>
2022-04-26 17:33:14 +0200

    Writting tests

--- lettings/tests/conftest.py
+++ lettings/tests/conftest.py
@@ -3,21 +3,12 @@ from lettings.models import Letting, Address
 
 
 @pytest.fixture
-def address_data():
-    return {'number': '1', 'street': 'street_name', 'city': 'city_name', 'state': 'state_name',
-            'zip_code': '1', 'country_iso_code': 'iso_code'}
-
+def letting_data():
+    return {'title': 'title_name'}
 
 @pytest.fixture
 def letting_data():
     return {'title': 'title_name'}
-
-
-@pytest.fixture
-def create_test_address(address_data):
-    address = Address.objects.create(**address_data)
-    return address
-
 
 @pytest.fixture
 def create_test_letting(letting_data, address_data):
--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,5 +1,4 @@
 import os
-import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -73,9 +72,6 @@ DATABASES = {
     }
 }
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
-db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
-DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
